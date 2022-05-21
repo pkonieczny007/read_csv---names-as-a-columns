001b3ad25873be452b49c3e6a10f95c217ff6210
pkonieczny007 <50171535+pkonieczny007@users.noreply.github.com>
2022-05-21 21:42:57 +0200

    df = pd.read_csv(data, names = kolumny)
    kolumny = ['names', 'pcs', 'o'] - parameter which making collumns to  pandas DataFrame

---
+++ read_csv-test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 21 21:23:03 2022
+
+@author: pkoni
+"""
+
+import pandas as pd 
+kolumny = ['names','szt','0'] #making header to csv_file
+df = pd.read_csv('1.txt',names=kolumny)
+
+
